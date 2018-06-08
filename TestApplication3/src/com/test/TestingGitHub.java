1ef8d495af85c78ffc2fd77f174a4832bc23805b
kirru12 <39980987+kirru12@users.noreply.github.com>
2018-06-08 11:55:19 +0530

    Update TestingGitHub.java

--- TestApplication3/src/com/test/TestingGitHub.java
+++ TestApplication3/src/com/test/TestingGitHub.java
@@ -3,7 +3,7 @@ package com.test;
 public class TestingGitHub {
 
 	public static void main(String[] args) {
-     System.out.println("anefsdiguk sdfuj");
+     System.out.println("kuthe ");
 	}
 
 }
